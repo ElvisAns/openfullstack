c6915fe09b010ec4e7fb4982b1ca2aed970bdd67
ElvisAns <ansimapersic@gmail.com>
2022-05-06 02:23:56 +0200

    Add buttons components

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Statistic_Line = ({text,count}) =>{
+const Statistics = ({text,count}) =>{
 
   if(!count){
     return(
@@ -17,7 +17,26 @@ const Statistic_Line = ({text,count}) =>{
 }
 
 const Head=()=><div>Give feedback</div>
-const Buttons
+const Buttons=({callback,text})=><button onClick={callback}>{text}</button>
+const StatisticsOveral= ({data}) =>{
+  const {good,neutral,bad}=data
+  return(
+    <table>
+      <tr>
+        <th>All</th>
+        <td>{good}</td>
+      </tr>
+      <tr>
+        <th>Average</th>
+        <td>{neutral}</td>
+      </tr>
+      <tr>
+        <th>Positive</th>
+        <td>{bad}</td>
+      </tr>
+    </table>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -25,9 +44,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+
   return (
     <div>
       <Head />
+      <Buttons callback={setGood} text="Good" />
+      <Buttons callback={setNeutral} text="Neutral" />
+      <Buttons callback={setBad} text="Bad" />
+
+      <Statistics text="Good" count={good} />
+      <Statistics text="Neutral" count={neutral} />
+      <Statistics text="Bad" count={bad} />
+      <StatisticsOveral data={{good:good,neutral:neutral,bad:bad}} />
     </div>
   )
 }
