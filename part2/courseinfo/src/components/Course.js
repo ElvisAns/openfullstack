0285d3131ac5a326c892c32e7d3dd91c37bee255
ElvisAns <ansimapersic@gmail.com>
2022-07-10 07:51:08 +0200

    [UPD] Modularize everything

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -7,7 +7,7 @@ const Header = (props)=>{
 const Content = ({parts})=>{
     return (
         <div>
-            {parts.map((val,i) =><p key={i}>{val.name} {val.exercises}</p>)}
+            {parts.map((val,i) =><p key={i}>{val.name} <b style={global.number}>{val.exercises}</b></p>)}
         </div>
     )
 }
@@ -17,13 +17,13 @@ const Total= ({parts}) => {
     const list = parts.map(el=>el.exercises)
     const total = list.reduce((prev,next)=>prev+next)
     return (
-        <p style={global.total}>total of {total} exercises</p>
+        <p style={global.total}>total of <i style={global.number}>{total}</i> exercises</p>
     )
 }
 
 const Course=({course})=>{
     return (
-        <div>{
+        <div style={global.body}>{
             course.map(val=>{
                 return(
                     <div key={val.id}>
--- part2/courseinfo/src/styles/global.js
+++ part2/courseinfo/src/styles/global.js
@@ -3,7 +3,16 @@ const global={
         {
             fontWeight:'900',
             color:'blue'
-        }
+        },
+    body:{
+        backgroundColor:'#eee',
+        padding:'90px',
+        borderRadius:'10px'
+    },
+    number:{
+        fontWeight:'900',
+        color:'green'
+    }
 }
 
 export default global
