4ee10a51357449632e74d134a8e1d944051ba0e7
ElvisAns <ansimapersic@gmail.com>
2022-08-28 15:33:18 +0200

    Added the form component

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,40 +1,51 @@
 import global from "../styles/global"
-import {useState} from "react"
+import { useState } from "react"
 
-const Header = (props)=>{
+const Header = (props) => {
     return (<h1>{props.course}</h1>)
 }
 
-const Content = ({parts})=>{
+const Form = (props) => {
+    return (
+        <input type="text"></input>
+    )
+}
+
+const Content = ({ parts }) => {
     return (
         <div>
-            {parts.map((val,i) =><p key={i}>{val.name} <b style={global.number}>{val.exercises}</b></p>)}
+            {parts.map((val, i) => <p key={i}>{val.name} <b style={global.number}>{val.exercises}</b></p>)}
         </div>
     )
 }
 
-const Total= ({parts}) => {
-    const list = parts.map(el=>el.exercises)
-    const total = list.reduce((prev,next)=>prev+next)
+const Total = ({ parts }) => {
+    const list = parts.map(el => el.exercises)
+    const total = list.reduce((prev, next) => prev + next)
     return (
         <p style={global.total}>total of <i style={global.number}>{total}</i> exercises</p>
     )
 }
 
-const Course=(props)=>{
-    const [course,updateCourses]=useState(props.course)
+const Course = (props) => {
+    const [course, updateCourses] = useState(props.course)
 
+    const addCourses = (event) => {
+        const prev = { ...course }
+        updateCourses(prev)
+    }
     return (
-        <div style={global.body}>{
-            course.map(val=>{
-                return(
-                    <div key={val.id}>
-                        <Header course={val.name} />
-                        <Content parts={val.parts} />
-                        <Total parts={val.parts}/>
-                    </div>
-                )
-            })
+        <div style={global.body}>
+            <Form onsubmit={addCourses} />{
+                course.map(val => {
+                    return (
+                        <div key={val.id}>
+                            <Header course={val.name} />
+                            <Content parts={val.parts} />
+                            <Total parts={val.parts} />
+                        </div>
+                    )
+                })
             }
         </div>
     )
