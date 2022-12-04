6de9334d8141cc864fbaa3e430b948c09e7ac78e
ElvisAns <ansimapersic@gmail.com>
2022-12-04 22:03:33 +0100

    feat : the form to save datas

---
+++ part2/phonebook/src/components/AddForm.js
@@ -0,0 +1,16 @@
+const AddForm = (props) => {
+    return(
+        <form onSubmit={props.saveUser}>
+            <label>Person Name</label>
+            <input onChange={props.setNewPerson} value={props.newPerson} placeholder="John Doe" type="text"/>
+
+            <label>Telephone</label>
+            <input onChange={props.setNewPhone} value={props.newPhone} placeholder="+1..." type="tel"/>
+
+            <button>save</button>
+
+        </form>
+    )
+}
+
+export default AddForm
