106913c174d54254c9602820bdc009cbf3f9d4ce
ElvisAns <ansimapersic@gmail.com>
2022-12-04 20:04:56 +0100

    enh : add the form handling logic

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -7,13 +7,13 @@ const Header = (props) => {
 
 const Form = (props) => {
     return (
-        <form onSubmit={props.addCourses}  style={props.style}>
-            <label style={props.style.label}>Course Name</label>
-            <input style={props.style.input} type="text" placeholder="course name" value={props.coursename} onChange={props.setCourseName} />
+        <form onSubmit={props.addCourses} style={props.style}>
             <label style={props.style.label}>Program Name</label>
-            <input style={props.style.input} type="text" placeholder="program name"  value={props.courseprogram} onChange={props.setCourseProgramName} />
-            <label style={props.style.label}>credits</label>
-            <input style={props.style.input} type="number" placeholder="credits"  value={props.coursecredit} onChange={props.setCoursCredit} />
+            <input style={props.style.input} type="text" placeholder="program name" value={props.coursename} onChange={props.setCourseName} />
+            <label style={props.style.label}>Part Name</label>
+            <input style={props.style.input} type="text" placeholder="part name" value={props.courseprogram} onChange={props.setCourseProgramName} />
+            <label style={props.style.label}>Exercises</label>
+            <input style={props.style.input} type="number" placeholder="exercices" value={props.coursecredit} onChange={props.setCoursCredit} />
 
             <button style={props.style.button} type="submit">Record</button>
         </form>
@@ -38,34 +38,60 @@ const Total = ({ parts }) => {
 
 const Course = (props) => {
     const [course, updateCourses] = useState(props.course)
-    const [coursename,updateCourseName]=useState("Elvis Ansima")
-    const [courseprogram,updateCourseProgram]=useState("")
-    const [coursecredit,updateCourseCredit]=useState("")
+    const [coursename, updateCourseName] = useState("Redux")
+    const [courseprogram, updateCourseProgram] = useState("Half Stack application development")
+    const [coursecredit, updateCourseCredit] = useState("10")
 
     const addCourses = (event) => {
         event.preventDefault()
         const prev = [...course]
         /*
-        * Handle search and update or push to the object depending
+        * Handle search and update or push to the object depending on whever the course is new or not
         */
+        const search = prev.find(program => program.name === coursename)
+        if (search) {
+            const pos = prev.indexOf(search);
+            prev[pos].parts.push({
+                name: coursename,
+                exercises: coursecredit,
+                id: prev[pos].parts.length + 1
+            })
+        }
+        else {
+            prev.push(
+                {
+                    name: coursename,
+                    id: prev.length + 1,
+                    parts: [
+                        {
+                            name: coursename,
+                            exercises: coursecredit,
+                            id: 0
+                        }
+                    ]
+                }
+            )
+        }
+
         updateCourses(prev)
     }
 
-    const setCoursCredit = event =>{
+    const setCoursCredit = event => {
         updateCourseCredit(event.target.value)
     }
 
-    const setCourseProgramName = event=>{
+    const setCourseProgramName = event => {
         updateCourseProgram(event.target.value)
     }
 
-    const setCourseName = event=>{
+    const setCourseName = event => {
         updateCourseName(event.target.value)
     }
 
     return (
         <div style={global.body}>
-            <Form setCoursCredit={setCoursCredit} setCourseProgramName={setCourseProgramName} setCourseName={setCourseName} courseprogram={courseprogram} coursename={coursename}  addCourses={addCourses} coursecredit={coursecredit} style={global.form}/>{
+            <Form setCoursCredit={setCoursCredit} setCourseProgramName={setCourseProgramName} setCourseName={setCourseName} courseprogram={courseprogram} coursename={coursename} addCourses={addCourses} coursecredit={coursecredit} style={global.form} />
+            {
                 course.map(val => {
                     return (
                         <div key={val.id}>
