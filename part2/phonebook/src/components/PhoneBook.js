5432035a9448519a30eb2559c43b74d5c2c6b598
ElvisAns <ansimapersic@gmail.com>
2022-12-05 11:57:49 +0100

    enh : add empty message in table when no datas

--- part2/phonebook/src/components/PhoneBook.js
+++ part2/phonebook/src/components/PhoneBook.js
@@ -1,11 +1,37 @@
 const Phonebook = (props) => {
     const list = props.persons
+    if (list.length === 0) {
+        return (
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Names</th>
+                        <td>Phone Number</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colSpan={2}>Phone book empty</td>
+                    </tr>
+                </tbody>
+            </table>
+        )
+    }
     return (
-        <div>
-            {
-                list.map((v)=>(<li>{v.name} : {v.phone}</li>))
-            }
-        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Names</th>
+                    <td>Phone Number</td>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    list.map((v) => (<tr><td>{v.name}</td><td>{v.phone}</td></tr>))
+                }
+            </tbody>
+        </table>
     )
 }
 
