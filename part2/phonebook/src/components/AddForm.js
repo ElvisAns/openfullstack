7bd642e57a21e48737b4748ba8329c37db1baf9a
ElvisAns <ansimapersic@gmail.com>
2022-12-05 12:19:58 +0100

    enh : add title to the form

--- part2/phonebook/src/components/AddForm.js
+++ part2/phonebook/src/components/AddForm.js
@@ -1,15 +1,18 @@
 const AddForm = (props) => {
-    return(
-        <form onSubmit={props.saveUser}>
-            <label>Person Name</label>
-            <input onChange={props.setNewPerson} value={props.newPerson} placeholder="John Doe" type="text"/>
+    return (
+        <>
+            <h3>Add new</h3>
+            <form onSubmit={props.saveUser}>
+                <label>Person Name</label>
+                <input onChange={props.setNewPerson} value={props.newPerson} placeholder="John Doe" type="text" />
 
-            <label>Telephone</label>
-            <input onChange={props.setNewPhone} value={props.newPhone} placeholder="+1" type="tel"/>
+                <label>Telephone</label>
+                <input onChange={props.setNewPhone} value={props.newPhone} placeholder="+1" type="tel" />
 
-            <button>save</button>
+                <button>save</button>
 
-        </form>
+            </form>
+        </>
     )
 }
 
