ae26b8bfe5c0a4cafe439b8218f649ea234f0a10
ElvisAns <ansimapersic@gmail.com>
2022-12-06 16:36:28 +0100

    feat : add the show more button action

--- part2/countrydatas/src/App.js
+++ part2/countrydatas/src/App.js
@@ -1,16 +1,23 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import CountryViewer from './components/CountryViewer';
 import axios from 'axios'
 import './App.css';
 
 function App() {
-  const [searchText,updateSearchText] = useState('');
-  const [countryData,updateCountryDatas] = useState([]);
+  const [searchText, updateSearchText] = useState('');
+  const [countryData, updateCountryDatas] = useState([]);
 
-  const loadCountryData = (event)=>{
+  const loadCountryData = (event) => {
     const search = event.target.value;
     updateSearchText(search)
-    axios.get(`https://restcountries.com/v3.1/name/${search}`).then(res=>{
+    axios.get(`https://restcountries.com/v3.1/name/${search}`).then(res => {
+      updateCountryDatas(res.data)
+    })
+  }
+
+  const loadSpecificCountry = name=>{
+    updateSearchText(name)
+    axios.get(`https://restcountries.com/v3.1/name/${name}`).then(res => {
       updateCountryDatas(res.data)
     })
   }
@@ -18,11 +25,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          <h1>Country Datas</h1>
-        </p>
-        <input placeholder="search" onChange={loadCountryData} value={searchText} type="search"/>
-        <CountryViewer countries={countryData} />
+        <h1>Country Datas</h1>
+        <input placeholder="search" onChange={loadCountryData} value={searchText} type="search" />
+        <CountryViewer loadSpecificCountry={loadSpecificCountry} countries={countryData} />
       </header>
     </div>
   );
