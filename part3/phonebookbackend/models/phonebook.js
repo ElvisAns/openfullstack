710219d4bef07214805ad60e94c5e7336c40c704
ElvisAns <ansimapersic@gmail.com>
2023-01-12 21:27:46 +0100

    enh : add constrain to the document  schema

--- part3/phonebookbackend/models/phonebook.js
+++ part3/phonebookbackend/models/phonebook.js
@@ -4,24 +4,36 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const phoneBookSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+    name: {
+        type: String,
+        required: true,
+        minLength: 3
+    },
+    number: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\d{5,}$/.test(v);
+            },
+        }
+    },
 })
 
 phoneBookSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString() //we create the id column and remove _id
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString() //we create the id column and remove _id
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
 module.exports = mongoose.model('phoneBook', phoneBookSchema)
