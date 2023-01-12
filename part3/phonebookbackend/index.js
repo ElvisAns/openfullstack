ba4a65209b09d4189e8421a65105163a44f01da7
ElvisAns <ansimapersic@gmail.com>
2023-01-12 17:03:29 +0100

    fix : well format the error message to follow the default schema

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -30,7 +30,7 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message);
 
     if (error.name === "CastError") {
-        return response.status(400).send({ error: "malformatted id" });
+        return response.status(400).send({ message: "malformatted id" });
     }
 
     next(error);
