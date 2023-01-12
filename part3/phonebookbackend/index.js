7342368c7752d753634c058140921a30494725a0
ElvisAns <ansimapersic@gmail.com>
2023-01-12 21:56:54 +0100

    feat : apply the linting and format the code

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,155 +1,154 @@
-require("dotenv").config();
-const express = require("express");
-const fs = require("fs");
-const crypto = require("crypto");
-const morgan = require("morgan");
-const db = require("./db.json");
-const cors = require("cors");
-const phoneBook = require("./models/phonebook");
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const phoneBook = require('./models/phonebook')
 
-const app = express();
-app.use(express.static("build"));
-app.use(express.json());
-app.use(cors());
-morgan.token("request-body", function (req, res) {
-    return JSON.stringify(req.body);
-});
+const app = express()
+app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
+morgan.token('request-body', function (req) {
+	return JSON.stringify(req.body)
+})
 
 app.use(
-    morgan(
-        ":method :url :status :res[content-length] - :response-time ms :request-body",
-        {
-            skip: function (req, res) {
-                return req.method !== "POST";
-            },
-        }
-    )
-);
+	morgan(
+		':method :url :status :res[content-length] - :response-time ms :request-body',
+		{
+			skip: function (req) {
+				return req.method !== 'POST'
+			},
+		}
+	)
+)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
+	console.error(error.message)
 
-    if (error.name === "CastError") {
-        return response.status(400).send({ message: "malformatted id" });
-    }
-    else if(error.name=== "ValidationError"){
-        return response.status(400).send({message:error.message})
-    }
+	if (error.name === 'CastError') {
+		return response.status(400).send({ message: 'malformatted id' })
+	}
+	else if(error.name=== 'ValidationError'){
+		return response.status(400).send({message:error.message})
+	}
 
-    next(error);
-};
+	next(error)
+}
 
 
-app.get("/api/persons", (request, response) => {
-    phoneBook.find({}).then((res) => {
-        response.json(res);
-    });
-});
+app.get('/api/persons', (request, response) => {
+	phoneBook.find({}).then((res) => {
+		response.json(res)
+	})
+})
 
-app.get("/info", (request, response) => {
-    const date = new Date();
-    phoneBook.count({}).then((count) => {
-        response.send(
-            `<h3>PhoneBook has info for ${count} people(s)</h3><br><h4>${date}</h4>`
-        );
-    });
-});
+app.get('/info', (request, response) => {
+	const date = new Date()
+	phoneBook.count({}).then((count) => {
+		response.send(
+			`<h3>PhoneBook has info for ${count} people(s)</h3><br><h4>${date}</h4>`
+		)
+	})
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
-    const id = request.params.id;
-    const person = phoneBook
-        .findById(id)
-        .then((person) => {
-            if (person) response.json(person);
-            else {
-                const notFound = { response: "The person id not found in phone book" };
-                response.status(404).send(notFound).end();
-            }
-        })
-        .catch((err) => next(err))
-});
+app.get('/api/persons/:id', (request, response, next) => {
+	const id = request.params.id
+	phoneBook
+		.findById(id)
+		.then((person) => {
+			if (person) response.json(person)
+			else {
+				const notFound = { response: 'The person id not found in phone book' }
+				response.status(404).send(notFound).end()
+			}
+		})
+		.catch((err) => next(err))
+})
 
-app.delete("/api/persons/:id",  (request, response, next) => {
-    const id = request.params.id;
-    const person = phoneBook
-        .findByIdAndDelete(id)
-        .then((person) => {
-            if (person)
-                response.send({ response: `success deleting the user with id ${id}` });
-            else {
-                const notFound = { response: "The person id not found in phone book" };
-                response.status(404).send(notFound).end();
-            }
-        })
-        .catch((err) => next(err))
-});
+app.delete('/api/persons/:id',  (request, response, next) => {
+	const id = request.params.id
+	phoneBook
+		.findByIdAndDelete(id)
+		.then((person) => {
+			if (person)
+				response.send({ response: `success deleting the user with id ${id}` })
+			else {
+				const notFound = { response: 'The person id not found in phone book' }
+				response.status(404).send(notFound).end()
+			}
+		})
+		.catch((err) => next(err))
+})
 
-app.post("/api/persons", (request, response, next) => {
-    const name = request.body.name;
-    const number = request.body.number;
+app.post('/api/persons', (request, response, next) => {
+	const name = request.body.name
+	const number = request.body.number
 
-    phoneBook.find({ name: name }).then((res) => {
-        if (res.length > 0) {
-            response
-                .status(400)
-                .json({ response: "A similar name is already recorded" })
-                .end();
-        } else {
-            const person = new phoneBook({
-                name,
-                number,
-            });
-            person
-                .save()
-                .then((res) => {
-                    response.send({ response: `success add the user to the phonebook` });
-                })
-                .catch((err) => next(err))
-        }
-    });
-});
+	phoneBook.find({ name: name }).then((res) => {
+		if (res.length > 0) {
+			response
+				.status(400)
+				.json({ response: 'A similar name is already recorded' })
+				.end()
+		} else {
+			const person = new phoneBook({
+				name,
+				number,
+			})
+			person
+				.save()
+				.then(() => {
+					response.send({ response: 'success add the user to the phonebook' })
+				})
+				.catch((err) => next(err))
+		}
+	})
+})
 
-app.patch("/api/persons/:id", (request, response, next) => {
-    const name = request.body.name;
-    const number = request.body.number;
-    const id = request.params.id;
+app.patch('/api/persons/:id', (request, response, next) => {
+	const name = request.body.name
+	const number = request.body.number
+	const id = request.params.id
 
-    const person = phoneBook
-        .findById(id)
-        .then((person) => {
-            if (person) {
-                //person is recorded
+	phoneBook
+		.findById(id)
+		.then((person) => {
+			if (person) {
+				//person is recorded
 
-                if (number !== undefined) {
-                    person.number = number;
-                }
+				if (number !== undefined) {
+					person.number = number
+				}
 
-                if (name !== undefined) {
-                    person.name = name;
-                }
+				if (name !== undefined) {
+					person.name = name
+				}
 
-                person
-                    .save()
-                    .then((res) => {
-                        response.send({
-                            response: `success updated the user with id ${id} to the phonebook`,
-                        });
-                    })
-                    .catch((err) => next(err));
-            } else {
-                const notFound = { response: "The person id not found in phone book" };
-                response.status(404).send(notFound).end();
-                process.exit(1);
-            }
-        })
-        .catch((err) => next(err));
-});
+				person
+					.save()
+					.then(() => {
+						response.send({
+							response: `success updated the user with id ${id} to the phonebook`,
+						})
+					})
+					.catch((err) => next(err))
+			} else {
+				const notFound = { response: 'The person id not found in phone book' }
+				response.status(404).send(notFound).end()
+				// eslint-disable-next-line no-undef
+				process.exit(1)
+			}
+		})
+		.catch((err) => next(err))
+})
 
 
 // this has to be the last loaded middleware.
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+	console.log(`Server running on port ${PORT}`)
+})
--- part3/phonebookbackend/models/phonebook.js
+++ part3/phonebookbackend/models/phonebook.js
@@ -1,39 +1,40 @@
 const mongoose = require('mongoose')
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+	.then(() => {
+		console.log('connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message)
+	})
 
 const phoneBookSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minLength: 3
-    },
-    number: {
-        type: String,
-        required: true,
-        validate: {
-            validator: function (v) {
-                return /^\d{2,3}-\d{5,}$/.test(v);
-            },
-        }
-    },
+	name: {
+		type: String,
+		required: true,
+		minLength: 3
+	},
+	number: {
+		type: String,
+		required: true,
+		validate: {
+			validator: function (v) {
+				return /^\d{2,3}-\d{5,}$/.test(v)
+			},
+		}
+	},
 })
 
 phoneBookSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString() //we create the id column and remove _id
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString() //we create the id column and remove _id
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
 })
 
 module.exports = mongoose.model('phoneBook', phoneBookSchema)
