86ef8000e06d894310660b14e558a300b6cdf203
ElvisAns <ansimapersic@gmail.com>
2023-04-07 17:51:56 +0200

    test : some  jest to the repo

--- part4/utils/for_testing.js
+++ part4/utils/for_testing.js
@@ -5,12 +5,14 @@ const reverse = (string) => {
     .join('')
 }
 
-const average = (array) => {
+const average = array => {
   const reducer = (sum, item) => {
     return sum + item
   }
 
-  return array.reduce(reducer, 0) / array.length
+  return array.length === 0
+    ? 0
+    : array.reduce(reducer, 0) / array.length
 }
 
 module.exports = {
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,65 @@
+var _ = require("lodash");
+
 const dummy = (blogs) => {
-    // ...
+  // ...
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs[0].likes;
+};
+
+const favoriteBlog = (blogs) => {
+  let favoriteBlogOb = blogs[0];
+  const len = blogs.length - 1;
+  for (let i = 1; i < len; i++) {
+    const b1 = blogs[i];
+    if (favoriteBlogOb.likes < b1.likes) {
+      favoriteBlogOb = { ...b1 };
+    }
   }
-  
-  module.exports = {
-    dummy
-  }
+  return {
+    title: favoriteBlogOb.title,
+    author: favoriteBlogOb.author,
+    likes: favoriteBlogOb.likes,
+  };
+};
+
+const mostBlogs = (blogs) => {
+  const authors = _.groupBy(blogs, "author");
+  const blogsNumber = _.orderBy(
+    authors,
+    [(author_posts) => author_posts.length],
+    ["desc"]
+  );
+  const mostBlog = blogsNumber[0];
+  return {
+    author: mostBlog[0].author,
+    blogs: mostBlog.length,
+  };
+};
+
+const mostLiked = (blogs) => {
+  const authors = _.groupBy(blogs, "author");
+  const author_and_likes = _.map(authors, (blogs, author) => {
+    const total = _.reduce(blogs, (result, value) => result + value.likes, 0);
+    return {
+      author,
+      likes: total,
+    };
+  });
+  const author_and_likes_ordered = _.orderBy(
+    author_and_likes,
+    ["likes"],
+    ["desc"]
+  );
+  return author_and_likes_ordered[0];
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLiked,
+};
