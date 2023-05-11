6a489ff849f4f85734fac7416f587233fac36f0c
ElvisAns <ansimapersic@gmail.com>
2023-05-11 22:03:28 +0200

    ref : better handle environement when testing vs prod vs dev

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
 const PORT =  3001
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test' ?process.env.MONGODB_TEST_URI:process.env.MONGODB_URI
 
 module.exports = {PORT,MONGODB_URI}
