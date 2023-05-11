c008f6e9f5ffd641e09ce6f3bae7dab51476b337
ElvisAns <ansimapersic@gmail.com>
2023-05-11 23:33:48 +0200

    enh : reformat the returned response

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -8,6 +8,14 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 const Blog = mongoose.model('Blog', blogSchema)
 
 module.exports = Blog
