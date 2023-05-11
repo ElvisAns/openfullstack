2caca86f42e4ffb4bd0ffe35cd483fc2d04cec9f
ElvisAns <ansimapersic@gmail.com>
2023-05-12 01:57:29 +0200

    feat : add neccessary routes for our blog app

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -3,15 +3,36 @@ const blogRouter =  Router();
 const Blog = require('../models/blog');
 const { result } = require('lodash');
 
-blogRouter.get('/', async (request, response,next) => {
-    const blogs = await Blog.find({})
-    response.json(blogs).end()
+blogRouter.get('/:id?', async (request, response,next) => {
+    if(!request.params.id){
+      const blogs = await Blog.find({})
+      response.json(blogs).end()
+    }
+    else{
+      const blog = await Blog.findById(request.params.id)
+      response.json(blog).end()
+    }
   })
   
 blogRouter.post('/', async (request, response,next) => {
+  if('title' in request.body){
     const blog = new Blog(request.body)
     const result = await blog.save()
     response.status(201).json(result).end()
+  }
+  else{
+    response.status(400).json({error:"Bad request, missing the title field"})
+  }
+})
+
+blogRouter.delete('/:id',async(request,response,next)=>{
+  res = await Blog.findByIdAndRemove(request.params.id)
+  response.status(201).json(res)
+})
+
+blogRouter.patch('/:id',async(request,response,next)=>{
+ res = await Blog.findByIdAndUpdate(request.params.id,request.body)
+ response.status(200).json(res)
 })
 
 module.exports =  blogRouter
