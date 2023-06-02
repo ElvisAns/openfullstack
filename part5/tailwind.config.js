0983eb1f5aa3704fb0b4d6126d3da326512e9eb9
ElvisAns <ansimapersic@gmail.com>
2023-06-02 04:18:46 +0200

    ref : remove white space in config file

--- part5/tailwind.config.js
+++ part5/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme'
-
 import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
