8615c750abecb7a6acdfb68cf1b9bc61a2ab5de3
ElvisAns <ansimapersic@gmail.com>
2023-06-02 04:21:35 +0200

    enh : import the user state and build dynamically navbar links

--- part5/src/components/common/NavBar.jsx
+++ part5/src/components/common/NavBar.jsx
@@ -1,6 +1,9 @@
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+import { useSelector } from 'react-redux'
 
 export default function NavBar() {
+  const userInfo = useSelector((state) => state.user.userInfo)
   return (<div className="navbar bg-base-100">
     <div className="navbar-start">
       <div className="dropdown">
@@ -9,17 +12,17 @@ export default function NavBar() {
         </label>
         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
           <li><NavLink to="/blogs" className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Blogs</NavLink></li>
-          <li tabIndex={0}>
+          {!userInfo.loggedIn && <li tabIndex={0}>
             <a className="justify-between">
               User
               <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
             </a>
             <ul className="p-2 bg-white">
-              <li><NavLink to='/login' className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Login</NavLink></li>
-              <li><NavLink to='/profile' className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Profile</NavLink></li>
+              {!userInfo.loggedIn && <li><NavLink to='/login' className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Login</NavLink></li>}
             </ul>
-          </li>
-          <li><NavLink to='/signin' className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Signup</NavLink></li>
+          </li>}
+          {userInfo.loggedIn && <li><NavLink to='/profile' className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Profile</NavLink></li>}
+          {!userInfo.loggedIn && <li><NavLink to='/signin' className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Signup</NavLink></li>}
         </ul>
       </div>
       <NavLink to='/' className="bg-transparent text-black btn btn-ghost normal-case text-xl" activeClassName='bg-blue-200'>Our Blogs</NavLink>
@@ -27,17 +30,17 @@ export default function NavBar() {
     <div className="navbar-center hidden lg:flex">
       <ul className="menu menu-horizontal px-1">
         <li><NavLink to="/blogs" className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Blogs</NavLink></li>
-        <li tabIndex={0}>
+        {!userInfo.loggedIn && <li tabIndex={0}>
           <a>
             User
             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
           </a>
           <ul className="p-2 bg-white">
-            <li><NavLink to='/login' className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Login</NavLink></li>
-            <li><NavLink to='/profile' className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Profile</NavLink></li>
+            {!userInfo.loggedIn && <li><NavLink to='/login' className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Login</NavLink></li>}
           </ul>
-        </li>
-        <li><NavLink to="/signin" className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Signup</NavLink></li>
+        </li>}
+        {userInfo.loggedIn && <li><NavLink to='/profile' className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Profile</NavLink></li>}
+        {!userInfo.loggedIn && <li><NavLink to="/signin" className="bg-transparent text-black hover:bg-blue-100" activeClassName='bg-blue-200'>Signup</NavLink></li>}
       </ul>
     </div>
     <div className="navbar-end">
