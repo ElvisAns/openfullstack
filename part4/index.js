0791e1a2845daf43bf7432fd96fee88874a56c51
ElvisAns <ansimapersic@gmail.com>
2023-06-06 00:10:36 +0200

    fix:add cors support

--- part4/index.js
+++ part4/index.js
@@ -1,7 +1,8 @@
 const { app, connectDB } = require("./app");
 const { MONGODB_URI, PORT } = require("./utils/config");
 const logger = require("./utils/logger");
-
+var cors = require('cors')
+app.use(cors())
 connectDB().then(() => {
   app.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`);
