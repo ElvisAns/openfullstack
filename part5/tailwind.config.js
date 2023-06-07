67bee811876f36ab3e2820213efafd3291f5c118
ElvisAns <ansimapersic@gmail.com>
2023-06-07 13:31:09 +0200

    feat : added blog form to create blog

--- part5/tailwind.config.js
+++ part5/tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 import daisyui from "daisyui";
+import forms from "@tailwindcss/forms";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Mulish', ...defaultTheme.fontFamily.sans],
+        sans: ["Mulish", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [daisyui],
+  plugins: [forms, daisyui],
   daisyui: {
     styled: true,
     themes: true,
