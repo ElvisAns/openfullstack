eef2152169edb294593a9f4ffe2eb97089258ef8
ElvisAns <ansimapersic@gmail.com>
2023-06-07 13:31:32 +0200

    feat : add scroll tracker

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -5,22 +5,32 @@ import Welcome from './components/Welcome'
 import Blogs from './components/Blogs'
 import Account from './components/Account'
 import ProtectedRoute from './components/common/ProtectedRoute'
-
+import { motion, useScroll } from "framer-motion"
 import Signin from './components/user/Signin'
 import Login from './components/user/Login'
 import './App.css'
+
+
+function ScrollIndicator() {
+  const { scrollYProgress } = useScroll();
+
+  return (
+    <motion.div className="progress-bar" style={{ scaleX: scrollYProgress }} />
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ScrollIndicator />
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route index path="/" element={<Welcome/>}></Route>
-          <Route path="/blogs" element={<Blogs/>}></Route>
+          <Route index path="/" element={<Welcome />}></Route>
+          <Route path="/blogs" element={<Blogs />}></Route>
           <Route element={<ProtectedRoute />}>
-            <Route path="/profile" element={<Account/>}></Route>
+            <Route path="/profile" element={<Account />}></Route>
           </Route>
           <Route path='/signin' element={<Signin />}></Route>
           <Route path='/login' element={<Login />}></Route>
